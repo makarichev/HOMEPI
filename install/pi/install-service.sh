1e5e6aa03be80cbfccd3f6c02fbd9a214b43091f
makarichev <dmitrij.makarichev@gmail.com>
2018-03-29 20:18:13 +0300

    1

--- install/pi/install-service.sh
+++ install/pi/install-service.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
-sudo cp -rf ./launcher.service  /lib/systemd/system;
-systemctl daemon-reload;
-systemctl enable launcher.service;
-systemctl start launcher.service;
 
+if [ "$#" -ne 1 ]
+then
+    echo "put unit name in argument"
+    exit 0
+fi
+  
 
+unit=$1
+filename="/home/pi/homepi/units/${unit}/index.js"
+
+if [ -f "$filename" ]
+then
+
+    sudo cat > /etc/systemd/system/${unit}.service << EOL
+[Unit]
+Description=HOMEPI ${unit} service
+After=multi-user.target
+
+[Service]
+Type=idle
+ExecStart=/usr/bin/node /home/pi/homepi/${unit}/index.js
+
+[Install]
+WantedBy=multi-user.target
+EOL
+    
+    sudo systemctl enable ${unit}.service
+    sudo systemctl start ${unit}.service
+
+else
+    echo "unit file ${filename} not found"
+fi
